1f13226ca6df24d45399123e64a504e909fe6931
Brent Lee Johnson <brentleejohnson73@gmail.com>
2021-07-21 14:18:25 +0200

    Added categories

--- app.py
+++ app.py
@@ -5,13 +5,22 @@ import requests
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def get_chuck_norris_jokes():
+@app.route("/", methods=["GET"])
+def chuck_norris_api_details():
 
     api_url = "https://api.chucknorris.io/jokes/random"
     response = requests.get(api_url).json()
 
     return "<strong>All details from Chuck Norris API: </strong><br>{}".format(response)
+
+
+@app.route("/categories")
+def chuck_norris_api_categories():
+
+    api_url = "https://api.chucknorris.io/jokes/categories"
+    response = requests.get(api_url).json()
+
+    return "<strong>Categories for Chuck Norris API:</strong><br>{}".format(response)
 
 
 if __name__ == "__main__":
